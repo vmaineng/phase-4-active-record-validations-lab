79cebc2222989fc4e8ec6a6f21af4cdae2461531
Mai Vang <vmaineng@gmail.com>
2022-06-30 13:55:10 -0500

    commit tree ee014e054c59c275ae7a0f16c9dff8b5cac57cd8

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250}
+    validates :summary, length: { maximum: 250}
+    validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  validate :exact_title
+
+
+
+
+# def  exact_title
+#     unless title.match?("You Won't Believe These True Facts")
+#         errors.add(:title, "False error")
+#     end
+# end
+
 end
